1c6a9835c1ef8fc06aaacf2bbe31f763864a42d2
ninhja <ariannaninh@gmail.com>
2025-02-05 12:57:32 -0800

    fixed videos not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
-import { useEffect, useState } from "react"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
-import SideBar from "./components/SideBar"
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
 
 function App() {
-  const [data, setData] = useState(null)
-  const [showModal, setShowModal] = useState(false)
+  const [data, setData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
-    setShowModal(!showModal)
+    setShowModal(!showModal);
   }
 
   useEffect(() => {
     async function fetchAPIData() {
-      const NASA_KEY = "[YOUR KEY HERE]"
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      const NASA_KEY = import.meta.env.VITE_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
       try {
-        const res = await fetch(url)
-        const apiData = await res.json()
-        console.log(apiData)
-        setData(apiData)
-        console.log('Fetched from API today')
+        const res = await fetch(url);
+        const apiData = await res.json();
+        console.log(apiData);
+        setData(apiData);
+        console.log("Fetched from API today");
       } catch (err) {
-        console.log(err.message)
+        console.log(err.message);
       }
     }
-    fetchAPIData()
-  }, [])
+    fetchAPIData();
+  }, []);
 
   return (
     <>
-      {data ? (<Main data={data} />) : (
+      {data ? (
+        <Main data={data} />
+      ) : (
         <div className="loadingState">
           <i className="fa-solid fa-gear"></i>
         </div>
@@ -38,11 +41,9 @@ function App() {
       {showModal && (
         <SideBar data={data} handleToggleModal={handleToggleModal} />
       )}
-      {data && (
-        <Footer data={data} handleToggleModal={handleToggleModal} />
-      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 export default function Main(props) {
-    const { data } = props
-    return (
-        <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
-        </div>
-    )
+  const { data } = props;
+  return (
+    <div className="imgContainer">
+      {data.media_type === "video" ? (
+        <iframe className="bgImage" src={data.url}></iframe>
+      ) : (
+        <img
+          src={data.hdurl}
+          alt={data.title || "bg-img"}
+          className="bgImage"
+        />
+      )}
+    </div>
+  );
 }
